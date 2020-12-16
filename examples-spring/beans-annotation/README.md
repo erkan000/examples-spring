f6d8a19ec1e12036c94591b97d3f2b7df2362b73
Erkan <erkan000@hotmail.com>
2020-12-16 19:41:04 +0300

    bean annotation

---
+++ examples-spring/beans-annotation/README.md
@@ -0,0 +1,7 @@
+### Spring Beans
+Bean Config with Java Annotations
+
+
+#### Usage:
+- App.java is Bean Scope Demo. First 2 object is singleton, and other two is Prototype
+- AppInjection.java is setter and constructor injection example
--- examples-spring/beans-annotation/src/main/java/start/App.java
+++ examples-spring/beans-annotation/src/main/java/start/App.java
@@ -1,19 +1,14 @@
 package start;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import config.AppConfig;
 import pojo.Person;
-import pojo.PersonDetail;
-import pojo.PersonLog;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		
 		Person service = context.getBean("myBean", Person.class);
 		System.out.println(service);
@@ -27,6 +22,8 @@ public class App {
 
 		Person service4 = context.getBean("myBeanProto", Person.class);
 		System.out.println(service4);
+		
+		context.close();
 	}
 
 }
