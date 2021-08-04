170ff64f12aad65e1694ff20489461c34aaa37fc
Erkan <erkan000@hotmail.com>
2021-08-04 22:24:20 +0300

    ..

--- mvc-thymeleaf/src/main/java/mvc/controller/FirstController.java
+++ mvc-thymeleaf/src/main/java/mvc/controller/FirstController.java
@@ -2,6 +2,8 @@ package mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
 import java.util.Map;
 
 @Controller
@@ -10,6 +12,7 @@ public class FirstController {
     @GetMapping("hello")
     public String hello(Map<String, Object> model) {
         model.put("message", "Hello World!");
+        model.put("today", Calendar.getInstance());
         return "thy";
     }
 
